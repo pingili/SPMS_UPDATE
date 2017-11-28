07fe4867e105360b1b2c0c594ba1f50cfd243160
Venkanna <venkat.5160@gmail.com>
2017-11-28 23:49:40 +0530

    receipts and payments

---
+++ BusinessEntities/ReceiptsAndPaymentsDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities
+{
+    public class ReceiptsAndPaymentsDto
+    {
+        public int Ahid { get; set; }
+        public string SLAccountHead { get; set; }
+        public double OpeningBalance { get; set; }
+        public double April { get; set; }
+        public double May { get; set; }
+        public double June { get; set; }
+        public double July { get; set; }
+        public double August { get; set; }
+        public double Sep { get; set; }
+        public double Oct { get; set; }
+        public double Nov { get; set; }
+        public double Dec { get; set; }
+        public double Jan { get; set; }
+        public double Feb { get; set; }
+        public double March { get; set; }
+
+    }
+
+    public class ReceiptsAndPaymentsListDto
+    {
+        public List<ReceiptsAndPaymentsDto> lstReceiptDto { get; set; }
+        public List<ReceiptsAndPaymentsDto> lstPaymentsDto { get; set; }
+    }
+
+}
--- MFIS.Web/Controllers/RecieptsAndPaymentsController.cs
+++ MFIS.Web/Controllers/RecieptsAndPaymentsController.cs
@@ -2,6 +2,8 @@
 using BusinessLogic;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,15 +12,77 @@ namespace MFIS.Web.Controllers
 {
     public class RecieptsAndPaymentsController : BaseController
     {
-        //
         // GET: /RecieptsAndPayments/
-
+        SqlConnection con = new SqlConnection(DataLogic.DBConstants.MFIS_CS);
         public ActionResult ReceiptsAndPayments()
         {
             OrganizationService objIOrganizationService = new OrganizationService();
             OrganizationDto organizationDto = objIOrganizationService.GetAll();
             ViewBag.OrganizationDetails = organizationDto;
-            return View();
+            ReceiptsAndPaymentsDto reptsAndpmts = new ReceiptsAndPaymentsDto();
+            ReceiptsAndPaymentsListDto lstreptsAndpmts = GetAllReceiptsAndPayments();
+            return View(lstreptsAndpmts);
+        }
+
+        public ReceiptsAndPaymentsListDto GetAllReceiptsAndPayments()
+        {
+            List<ReceiptsAndPaymentsDto> lst = new List<ReceiptsAndPaymentsDto>();
+            int GroupId = GroupInfo.GroupID;
+            SqlCommand cmd = new SqlCommand("uspGroupReceiptAndPayments", con);
+            cmd.Parameters.Add(new SqlParameter("GroupID", GroupId));
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandTimeout = 0;
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            ReceiptsAndPaymentsListDto objdto = new ReceiptsAndPaymentsListDto();
+            
+            while (dr.Read())
+            {
+                ReceiptsAndPaymentsDto obj = new ReceiptsAndPaymentsDto();
+                obj.Ahid = dr["Ahid"] == DBNull.Value ? default(Int32) : Convert.ToInt32(dr["Ahid"]);
+                obj.SLAccountHead = dr["SLAccountHead"] == DBNull.Value ? string.Empty : Convert.ToString(dr["SLAccountHead"]);
+                obj.OpeningBalance = dr["OpeningBalance"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["OpeningBalance"]);
+                obj.April = dr["Apr"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Apr"]);
+                obj.May = dr["May"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["May"]);
+                obj.June = dr["Jun"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Jun"]);
+                obj.July = dr["Jul"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Jul"]);
+                obj.August = dr["Aug"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Aug"]);
+                obj.Sep = dr["Sep"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Sep"]);
+                obj.Oct = dr["Oct"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Oct"]);
+                obj.Nov = dr["Nov"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Nov"]);
+                obj.Dec = dr["Dec"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Dec"]);
+                obj.Jan = dr["Jan"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Jan"]);
+                obj.Feb = dr["Feb"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Feb"]);
+                obj.March = dr["Mar"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Mar"]);
+
+                lst.Add(obj);
+            }
+            objdto.lstReceiptDto = lst;
+            while (dr.NextResult())
+            {
+                while (dr.Read())
+                {
+                    ReceiptsAndPaymentsDto obj = new ReceiptsAndPaymentsDto();
+                    obj.Ahid = dr["Ahid"] == DBNull.Value ? default(Int32) : Convert.ToInt32(dr["Ahid"]);
+                    obj.SLAccountHead = dr["SLAccountHead"] == DBNull.Value ? string.Empty : Convert.ToString(dr["SLAccountHead"]);
+                    obj.OpeningBalance = dr["OpeningBalance"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["OpeningBalance"]);
+                    obj.April = dr["Apr"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Apr"]);
+                    obj.May = dr["May"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["May"]);
+                    obj.June = dr["Jun"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Jun"]);
+                    obj.July = dr["Jul"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Jul"]);
+                    obj.August = dr["Aug"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Aug"]);
+                    obj.Sep = dr["Sep"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Sep"]);
+                    obj.Oct = dr["Oct"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Oct"]);
+                    obj.Nov = dr["Nov"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Nov"]);
+                    obj.Dec = dr["Dec"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Dec"]);
+                    obj.Jan = dr["Jan"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Jan"]);
+                    obj.Feb = dr["Feb"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Feb"]);
+                    obj.March = dr["Mar"] == DBNull.Value ? default(double) : Convert.ToDouble(dr["Mar"]);
+                }
+            }
+            objdto.lstPaymentsDto = lst;
+            return objdto;
+
         }
 
     }
