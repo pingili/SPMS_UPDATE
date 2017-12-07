3e8eb2194e5454d81436599f4fd9df9f688e1bcf
chandan pingili <chandanp.net@outlook.com>
2017-12-07 22:29:17 +0530

    Receipts and payments scripts

--- DBScripts/ReceiptsAndPayments.sql
+++ DBScripts/ReceiptsAndPayments.sql
@@ -3,28 +3,28 @@ ALTER Proc [dbo].[uspGroupReceiptAndPayments]
 --Exec [uspGroupReceiptAndPayments] 282
 AS
 BEGIN
+if OBJECT_ID('tempdb..#Receipts') is not null drop table #receipts
+if OBJECT_ID('tempdb..#Payments') is not null drop table #Payments
 
-CREATE TABLE Receipts(AHID int ,SLAccountHead varchar(200),OpeningBalance decimal, TotalByMonth decimal,monthname varchar(10 ))
-CREATE TABLE Payments(AHID int ,SLAccountHead varchar(200),OpeningBalance decimal, TotalByMonth decimal,monthname varchar(10 ))
+	CREATE TABLE #Receipts(AHID int ,SLAccountHead varchar(200),OpeningBalance decimal, TotalByMonth decimal,monthname varchar(10 ))
+	CREATE TABLE #Payments(AHID int ,SLAccountHead varchar(200),OpeningBalance decimal, TotalByMonth decimal,monthname varchar(10 ))
 ;with
- ct1 as(select AH.AHID, AH.AHName SLAccountHead, PAH.AHName  As GLAccountHead,Ah.OpeningBalance OpeningBalance,sum(DrAmount) total,AM.TransactionDate from AccountTransactions AT
-Join ReceiptAppropriationPriority RAP ON RAP.AHID=AT.AHID
-JOIN AccountHead AH ON AH.AHID=RAP.AHID AND RAP.StatusId = 1 AND AH.StatusID = 1
+ ct1 as(select * from AccountTransactions AT
+--Join ReceiptAppropriationPriority RAP ON RAP.AHID=AT.AHID
+where AccountMasterID in( 
+select AccountMasterID from AccountMaster where TransactionType in (
+select RefID from RefValueMaster where  refmasterid = 3027 and refcode in ('gop', 'GMR', 'GOR', 'GMP'))
+))
+, ct2 as(Select  AH.AHID, AH.AHName SLAccountHead, PAH.AHName  As GLAccountHead,Ah.OpeningBalance OpeningBalance,sum(DrAmount) total,AM.TransactionDate from ct1 c
+JOIN AccountHead AH ON AH.AHID=c.AHID AND  AH.StatusID = 1
 INNER JOIN AccountHead PAH ON AH.ParentAHID = PAH.AHID AND PAH.StatusId = 1
-JOIN AccountMaster AM ON AM.AccountMasterID=AT.AccountMasterID
+JOIN AccountMaster AM ON AM.AccountMasterID=c.AccountMasterID
 Where AM.GroupId=@GroupId
 Group By  AH.AHID, AH.AHName,PAH.AHName,AM.AccountMasterID,AM.TransactionDate,Ah.OpeningBalance)
 Insert into Receipts
-select AHID,SLAccountHead,OpeningBalance,sum(total),Month(TransactionDate) [Month]  from ct1 
+select AHID,SLAccountHead,OpeningBalance,sum(total),Month(TransactionDate) [Month]  from ct2 
 Group By AHID,SLAccountHead,TransactionDate,OpeningBalance
-Union All
-SELECT distinct SLAccountId AHID, SLNAME  AS AHNAME ,Ah.OpeningBalance OpeningBalance, SUM(DrAmount),Month(Am.TransactionDate) [Month] FROM VWGroupAccountTree VW
-JOIN AccountHead AH ON AH.AHID=VW.SLAccountId AND AH.StatusID = 1
- JOIN AccountTransactions AT ON AT.AHID=Vw.SLAccountId
-INNER JOIN AccountHead PAH ON AH.ParentAHID = PAH.AHID AND PAH.StatusId = 1
- JOIN AccountMaster AM ON AM.AccountMasterID=AT.AccountMasterID
-Where AM.GroupId=@GroupId AND  Vw.TransactionType in ('G','B')
-GROUP BY SLAccountId,SLNAME,AM.TransactionDate,AH.OpeningBalance
+
 
 ;with cte4 as(select Ahid,SLAccountHead
   ,isnull(OpeningBalance, 0) OpeningBalance
@@ -42,33 +42,31 @@ GROUP BY SLAccountId,SLNAME,AM.TransactionDate,AH.OpeningBalance
   ,isnull(sum(case when monthname = 12 then TotalByMonth end), 0) Dec
 from Receipts
 group by Ahid, SLAccountHead,OpeningBalance)
-select * from cte4
+select * from cte4 where SLAccountHead !='1'
 union all
 Select 0,'Total',sum(OpeningBalance),
 sum(Jan),sum(Feb),Sum(Mar),Sum(Apr) ,Sum(May),Sum(Jun),Sum(Jul),Sum(Aug),Sum(Sep),Sum(Oct),Sum(Nov),Sum(Dec)from cte4
---Order By Ahid
+where SLAccountHead !='1'
 
 ;with
- ct1 as(select AH.AHID, AH.AHName SLAccountHead, PAH.AHName  As GLAccountHead ,Ah.OpeningBalance OpeningBalance,sum(CrAmount) total,AM.TransactionDate from AccountTransactions AT
-Join ReceiptAppropriationPriority RAP ON RAP.AHID=AT.AHID
-JOIN AccountHead AH ON AH.AHID=RAP.AHID AND RAP.StatusId = 1 AND AH.StatusID = 1
+ ct1 as(select * from AccountTransactions AT
+--Join ReceiptAppropriationPriority RAP ON RAP.AHID=AT.AHID
+where AccountMasterID in( 
+select AccountMasterID from AccountMaster where TransactionType in (
+select RefID from RefValueMaster where  refmasterid = 3027 and refcode in ('gop', 'GMR', 'GOR', 'GMP'))
+))
+, ct2 as(Select  AH.AHID, AH.AHName SLAccountHead, PAH.AHName  As GLAccountHead,Ah.OpeningBalance OpeningBalance,sum(CrAmount) total,AM.TransactionDate from ct1 c
+JOIN AccountHead AH ON AH.AHID=c.AHID AND  AH.StatusID = 1
 INNER JOIN AccountHead PAH ON AH.ParentAHID = PAH.AHID AND PAH.StatusId = 1
-JOIN AccountMaster AM ON AM.AccountMasterID=AT.AccountMasterID
+JOIN AccountMaster AM ON AM.AccountMasterID=c.AccountMasterID
 Where AM.GroupId=@GroupId
 Group By  AH.AHID, AH.AHName,PAH.AHName,AM.AccountMasterID,AM.TransactionDate,Ah.OpeningBalance)
 Insert into Payments
-select AHID,SLAccountHead,OpeningBalance,sum(total),Month(TransactionDate) [Month]  from ct1 
-Group By AHID,SLAccountHead,OpeningBalance,TransactionDate
-Union All
-SELECT distinct SLAccountId AHID, SLNAME  AS AHNAME ,Ah.OpeningBalance OpeningBalance, SUM(CrAmount),Month(Am.TransactionDate) [Month] FROM VWGroupAccountTree VW
-JOIN AccountHead AH ON AH.AHID=VW.SLAccountId AND AH.StatusID = 1
- JOIN AccountTransactions AT ON AT.AHID=Vw.SLAccountId
-INNER JOIN AccountHead PAH ON AH.ParentAHID = PAH.AHID AND PAH.StatusId = 1
- JOIN AccountMaster AM ON AM.AccountMasterID=AT.AccountMasterID
-Where AM.GroupId=@GroupId AND  Vw.TransactionType in ('G','B')
-GROUP BY SLAccountId,SLNAME,Ah.OpeningBalance,AM.TransactionDate
+select AHID,SLAccountHead,OpeningBalance,sum(total),Month(TransactionDate) [Month]  from ct2 
+Group By AHID,SLAccountHead,TransactionDate,OpeningBalance
 
-;with cte3 as(select Ahid,SLAccountHead
+
+;with cte4 as(select Ahid,SLAccountHead
   ,isnull(OpeningBalance, 0) OpeningBalance
   ,isnull(sum(case when monthname = 1 then TotalByMonth end), 0) Jan
   ,isnull(sum(case when monthname = 2 then TotalByMonth end), 0) Feb 
@@ -84,11 +82,10 @@ GROUP BY SLAccountId,SLNAME,Ah.OpeningBalance,AM.TransactionDate
   ,isnull(sum(case when monthname = 12 then TotalByMonth end), 0) Dec
 from Payments
 group by Ahid, SLAccountHead,OpeningBalance)
-Select * from cte3
-UNION ALL 
+select * from cte4 where SLAccountHead !='1'
+union all
 Select 0,'Total',sum(OpeningBalance),
-sum(Jan),sum(Feb),Sum(Mar),Sum(Apr) ,Sum(May),Sum(Jun),Sum(Jul),Sum(Aug),Sum(Sep),Sum(Oct),Sum(Nov),Sum(Dec)from cte3
---Order By AHID
-Drop Table Receipts
-Drop Table Payments
+sum(Jan),sum(Feb),Sum(Mar),Sum(Apr) ,Sum(May),Sum(Jun),Sum(Jul),Sum(Aug),Sum(Sep),Sum(Oct),Sum(Nov),Sum(Dec)from cte4
+where SLAccountHead !='1'
+
 END
---
+++ DBScripts/SQLQuery8.sql
@@ -0,0 +1,10 @@
+
+insert into RefValueMaster (
+	RefMasterID,	RefCode,	RefValue,	Description,	DisplayOrder,	IsSeedValue,	IsActive,	CreatedBy,	CreatedOn,	ModifiedBy,	ModifiedOn) 
+Values(
+3027,	'CCW',	'ContraEntryCashWithDrawel',	'ContraEntryCashWithDrawel',	1	,1,	1,	1	,GetDate(),	NULL,	NULL)
+
+insert into RefValueMaster (
+	RefMasterID,	RefCode,	RefValue,	Description,	DisplayOrder,	IsSeedValue,	IsActive,	CreatedBy,	CreatedOn,	ModifiedBy,	ModifiedOn) 
+Values(
+3027,	'CCW',	'ContraEntryCashDeposit',	'ContraEntryCashDeposit',	1	,1,	1,	1	,GetDate(),	NULL,	NULL)
